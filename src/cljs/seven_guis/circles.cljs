88d22718863959702b772f204a6600793af6a78e
Tao Roa Lin <taoroalin@gmail.com>
2020-11-30 01:54:04 -0500

    Circles working

--- src/cljs/seven_guis/circles.cljs
+++ src/cljs/seven_guis/circles.cljs
@@ -2,10 +2,11 @@
 
 (def default-radius 50)
 
-(defn circle [{x :x y :y d :d filled? :filled?}]
+(defn circle [update id {x :x y :y d :d filled? :filled?}]
   (let [r (/ d 2)
         top (- y r)
         left (- x r)]
+    ^{:key id}
     [:div {:style {:position "absolute"
                    :top top
                    :left left
@@ -13,7 +14,8 @@
                    :height d
                    :border-radius 100000
                    :background-color (when filled? "lightgrey")
-                   :border "2px solid black"}}]))
+                   :border "2px solid black"}
+           :on-click #(update % id)}]))
 
 (defn create-circle [event base-element]
   (let [mouse-x (.-clientX event)
@@ -25,17 +27,39 @@
         y (- mouse-y base-y)]
     {:d default-radius :x x :y y :filled? false}))
 
+(defn drop-redos [{undos :undos history :history}]
+  {:undos 0 :history (drop undos history)})
+
+
 (defn circles []
-  (let [state (atom {:circles [{:x 100 :y 100 :d 100 :filled? true}]})
+  (let [state (atom {:history (list [{:x 100 :y 100 :d 100 :filled? true}])
+                     :undos 0})
         !canvas (atom nil)]
     (fn []
-      [:div [:div [:button {} "Undo"] [:button {} "Redo"]]
+      (print state)
+      [:div [:div
+             [:button {:on-click #(swap! state (fn [state]
+                                                 (assoc state :undos (min (- (count (:history state)) 1)
+                                                                          (inc (:undos state))))))}
+              "Undo"]
+             [:button {:on-click #(swap! state update :undos (fn [undos] (max 0 (dec undos))))} "Redo"]]
        [:div {:style {:width "500px"
                       :height "500px"
                       :position "relative"
                       :border "1px solid black"}
               :ref #(reset! !canvas %)
               :on-click (fn [event]
-                          (swap! state update-in [:circles]
-                                 #(conj % (create-circle event @!canvas))))}
-        (map circle (:circles @state))]])))
+                          (let [circle (create-circle event @!canvas)]
+                            (swap! state (fn [state]
+                                           (let [state (drop-redos state)
+                                                 none (print state)]
+                                             (assoc state :history
+                                                    (cons (conj (first (:history state)) circle)
+                                                          (:history state))))))))}
+        (map-indexed (partial circle
+                              (fn [event id]
+                                (.stopPropagation event)
+                                (swap! state #(let [state (drop-redos %)]
+                                                (assoc state :history (cons (assoc-in (first (:history state)) [id :filled?] true) (:history state)))))))
+                     (let [{history :history undos :undos} @state]
+                       (nth history undos)))]])))
--- src/cljs/seven_guis/common.cljs
+++ src/cljs/seven_guis/common.cljs
@@ -2,4 +2,8 @@
 
 (defn basic-text-input [name atom path]
   [:div [:label {:for name} name]
-   [:input {:id name :value (get-in (deref atom) path) :on-input (fn [event] (swap! atom #(assoc-in % path (-> event .-target .-value))))}]])
+   [:input {:id name
+            :value (get-in (deref atom) path)
+            :on-input (fn [event]
+                        (swap! atom #(assoc-in % path (-> event .-target .-value))))}]])
+
