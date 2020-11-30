66ad983d216ff4ecc94610b1879f9bdcb717f169
Tao Roa Lin <taoroalin@gmail.com>
2020-11-30 10:30:49 -0500

    documented some code

--- src/cljs/seven_guis/cells.cljs
+++ src/cljs/seven_guis/cells.cljs
@@ -1,17 +1,22 @@
 (ns seven-guis.cells (:require [reagent.core :refer [atom]]
                                [instaparse.core :as insta :refer-macros [defparser]]
                                [clojure.walk :refer [postwalk]]))
-
-(defparser string->ast "src/cells.insta")
-
+;; Constants
+(def cols (range 26))
+(def rows (range 100))
+(def letters (map (comp char #(+ 97 %)) cols))
 (def test-strings ["hello =1" "hello=$B1"])
 
-(def cols (range 26))
-(def letters (map (comp char #(+ 97 %)) cols))
-(def rows (range 100))
+;; generate parser from EBNF at compile time
+(defparser string->ast "src/cells.insta")
+
+
+
 (defn letter->idx [letter] (- (.charCodeAt letter 0) 65))
 
-(defn ast->ast [ast]
+(defn clean-ast
+  "convert numbers and operations to their own types, and convert letter cols to numbers"
+  [ast]
   (insta/transform
    {:number js/parseInt
     :op {"+" + "-" - "*" *}
@@ -22,7 +27,9 @@
               (letter->idx (str letter))))}
    ast))
 
-(defn ast->links [ast]
+(defn ast->links
+  "scrape all cell refs from the ast into a set"
+  [ast]
   (let [smoosh (fn [& args] (into #{} (mapcat #(if (set? %) % #{}) args)))]
     (insta/transform {:ref (fn [col row] #{[col row]})
                       :toplevel smoosh
@@ -43,12 +50,14 @@
     (update-in state [:cells pos]
                #(assoc (assoc % :number number) :display display))))
 
-;; This thing is beautiful!!!!!!
-;; should convert to breadth first
-;; improve
-(defn propagate [state pos]
-  (let [state (evaluate state pos)]
-    (reduce propagate state (get-in state [:cells pos :backlinks]))))
+;; Propagating with one reduction is beautiful!
+;; could improve to wait for all dependencies to update to improve performance
+(defn propagate [state pos seen]
+  (if (seen pos) (do (js/alert "you have created an infinite loop. please change it back") state)
+      (let [state (evaluate state pos)
+            seen (conj seen pos)
+            propagate #(propagate %1 %2 seen)]
+        (reduce propagate state (get-in state [:cells pos :backlinks])))))
 
 (defn add-backlinks [state pos]
   (assoc state :cells
@@ -58,7 +67,9 @@
                  (:cells state)
                  (get-in state [:cells pos :links]))))
 
-(defn remove-backlinks [state pos]
+(defn remove-backlinks
+  "remove backlinks from old equation"
+  [state pos]
   (assoc state :cells
          (reduce (fn [cells link]
                    (assoc-in cells [link :backlinks]
@@ -66,34 +77,46 @@
                  (:cells state)
                  (get-in state [:cells pos :links]))))
 
-(defn cells []
-  (let [state (atom {:cells {[1 0] {:number 1 :raw "1" :display "1" :links #{} :backlinks #{}}}
-                     :editing [0 0]})
-        set-cell (fn [pos string]
-                   (swap! state
-                          (fn [state]
-                            (let [ast (string->ast string)]
-                              (if (insta/failure? ast)
-                                state
-                                (let [ast (ast->ast ast)
-                                      links (ast->links ast)
-                                      _ (println ast)
-                                      equation? (coll? (second ast))
-                                      state (remove-backlinks state pos)
-                                      state (update-in state [:cells pos] #(merge % {:raw string
-                                                                                     :equation ast
-                                                                                     :links links
-                                                                                     :equation? equation?}))
-                                      state (add-backlinks state pos)
-                                      state (propagate state pos)]
-                                  state))))))
+(defn set-cell
+  "this function is in charge of swapping the cells after input.
+   it takes a string all the way to a parsed, 
+   evaluated, and propagated equation"
+  [state pos string]
+  (swap! state
+         (fn [state]
+           (let [ast (string->ast string)]
+             (if (insta/failure? ast) ;; don't update if input doesn't parse
+               state
+               (let [ast (clean-ast ast)
+                     links (ast->links ast)
+                     equation? (coll? (second ast))
+                     state (remove-backlinks state pos)
+                     state (update-in state [:cells pos]
+                                      #(merge % {:raw string
+                                                 :equation ast
+                                                 :links links
+                                                 :equation? equation?}))
+                     state (add-backlinks state pos)
+                     state (propagate state pos #{})]
+                 state))))))
 
-        cell (fn [cell pos]
-               ^{:key (pos 1)} [:td {:class (when (:equation? cell) "equation") :on-click #(swap! state assoc :editing pos)} (:display cell)])
-        editing-cell (fn [cell pos]
-                       ^{:key (pos 1)} [:td#editing [:input {:auto-focus true
-                                                             :default-value (:raw cell)
-                                                             :on-change #(set-cell pos (-> % .-target .-value))}]])]
+(defn cells
+  "it's a spreadsheet. it's virtue is it's less code than real spreadsheets"
+  []
+  (let
+   [state (atom {:cells {[1 0] {:number 1 :raw "1" :display "1" :links #{} :backlinks #{}}}
+                 :editing [0 0]})
+    cell (fn [cell pos]
+           ^{:key (pos 1)} [:td {:class (when (:equation? cell) "equation")
+                                 :on-click #(swap! state assoc :editing pos)}
+                            (:display cell)])
+    editing-cell (fn [cell pos]
+                   ^{:key (pos 1)}
+                   [:td#editing
+                    [:input {:auto-focus true
+                             :default-value (:raw cell)
+                             :on-change #(set-cell state pos (-> % .-target .-value))
+                             :on-key #(swap! state assoc :editing pos)}]])]
     (fn []
       (println "state" @state)
       [:div.cells
