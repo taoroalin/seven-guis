1afceda21da5b4ca8feebda3e7220db726a79f5f
Tao Roa Lin <taoroalin@gmail.com>
2020-12-01 17:31:59 -0500

    Finally got decent performance!

--- src/cljs/seven_guis/crud.cljs
+++ src/cljs/seven_guis/crud.cljs
@@ -1,101 +1,87 @@
 (ns seven-guis.crud
-  "Strategy:
-   1: only render elements on screen
-   2: filter with trie
-   filtering 100k takes 600ms
-   "
   (:require [reagent.core :refer [atom]]
+            [clojure.string]
             [seven-guis.common :refer [basic-text-input atom-text-input]]))
 
+(def sep "`")
 (def li-height 26.4)
+(def buffer "number of extra names to render" 40)
 
 (defn random-person []
-  (let [random-name #(str (rand-int 100000))
-        last (random-name)
-        full (str last "," (random-name))]
-    [last full]))
+  (let [random-name #(str (rand-int 100000))]
+    (str (random-name) sep (random-name))))
 
-(defn update-trie
-  "last name is key in trie, first name is added to the end"
-  [trie last-name full-name]
-  (update-in trie
-             (conj (vec last-name) :full)
-             #(conj (or % (sorted-set)) full-name)))
 
-(defn init-trie [names]
-  (reduce #(update-trie %1 (first %2) (second %2)) {} names))
+(defn render-person [state stored-name]
+  (let [rendered-name (clojure.string/replace stored-name sep ",")]
+    ^{:key stored-name}
+    [:li {:on-click #(swap! state assoc :selected stored-name)}
+     rendered-name]))
 
-(defn add-person [state]
-  (let [last (:last state)
-        full (str last "," (:first state))]
+(defn add-person [{people :people first :first last :last :as state}]
+  (let [person (str last sep first)]
     (-> state
-        (assoc :first "")
+        (assoc :filter "")
         (assoc :last "")
-        (update :trie #(update-trie % last full)))))
+        (assoc :people (conj people person))
+        (assoc :selected person))))
 
-(defn remove-person [state]
-  (let [{last-name :last first-name :first} (:selected-person state)
-        go (fn [node last-branch idx]
-             (if (< idx (count last-name)) ()))]
-    (update state :trie #(go % nil 0))))
+(defn remove-person [{selected :selected people :people :as state}]
+  (merge state {:selected nil :people (disj people selected)}))
 
-(defn subtrie [trie last-name]
-  (get-in trie (vec last-name)))
+(defn replace-person [state]
+  (-> state
+      (remove-person)
+      (add-person)))
 
-(defn render-person [full-name]
-  ^{:key full-name}
-  [:li full-name])
+(defn filter-people [{filter :filter people :people :as state}]
+  (let [filter-gen
+        (if (= filter "") (seq people)
+            (let [last-idx (- (count filter) 1)
+                  after-filter (str (subs filter 0 last-idx) (char (+ (.charCodeAt filter last-idx) 1)))
+                  _ (println "filter after" after-filter)]
+              (rsubseq people >= filter < after-filter)))]
+    (merge state {:filtered-generator filter-gen :filtered-vector []})))
 
-(defn list-people [trie]
-  (if (set? trie)
-    (map render-person trie)
-    (mapcat list-people (vals trie))))
+(defn scroll-load [{gen :filtered-generator vec :filtered-vector :as state} scroll]
+  (let [names-to-load (+ buffer (quot scroll li-height))
+        [extra gen] (split-at (- names-to-load (count vec)) gen)
+        vec (into vec extra)]
+    (merge state {:filtered-generator gen :filtered-vector vec})))
 
+(defn index [state]
+  (scroll-load (filter-people state) 0))
 
 (defn crud []
-  (let [state (atom {:filtered-people []
-                     :trie {}
+  (let [state (atom {:people (sorted-set "Lin`Tao")
+                     :filtered-generator ()
+                     :filtered-vector []
+                     :scroll 0
                      :filter ""
                      :first ""
                      :last ""
-                     :selected-person nil})
-        update-forcer (atom 0)
-        !box (clojure.core/atom nil)]
+                     :pad 0
+                     :pad-bottom 0
+                     :selected nil})]
     (fn []
-      ;; using extra atom to force rerender on scroll
-      ;; should be better way
-      @update-forcer
       [:div
        [:div {:style {:display "flex" :flex-direction "row"}}
         [:div
          [basic-text-input
           "Filter Last Name:"
           (:filter @state)
-          #(swap! state (fn [state] (merge state {:filtered-people (vec (list-people (subtrie (:trie state) %)))
-                                                  :filter %})))]
+          (fn [text] (swap! state (comp index #(assoc % :filter text))))]
          [:div {:style {:max-height "500px" :overflow "auto" :border "1px solid black"}
-                :ref #(when (not= @!box %) (reset! !box %))
-                :on-scroll #(swap! update-forcer inc)}
-          (let [{filtered-people :filtered-people
-                 selected-person :selected-person} @state
-                total-height (* li-height (count filtered-people))
-                show-height (min (* li-height (count filtered-people)) (if @!box (.-offsetHeight @!box) 500))
-                number-shown (min (count filtered-people) (int (/ show-height li-height)))
-                max-scroll (- total-height show-height)
-                scroll (if @!box (min max-scroll (.-scrollTop @!box)) 0)
-                bottom (- total-height (+ scroll show-height))
-                first-person (min (- (count filtered-people) number-shown) (int (/ scroll li-height)))
-                last-person (min (count filtered-people) (+ first-person number-shown))]
-            (list ^{:key 0} [:div {:style {:height (str scroll "px")}}]
-                  ^{:key 1} (seq (subvec filtered-people first-person last-person))
-                  ^{:key 2} [:div {:style {:height (str bottom "px")}}]))]]
+                :on-scroll #(swap! state scroll-load (-> % .-target .-scrollTop))}
+          (map (partial render-person state) (:filtered-vector @state))]
+         [:p "Scroll down to load more"]]
         [:div
          [atom-text-input "First Name:" state [:first]]
          [atom-text-input "Last Name:" state [:last]]]]
        [:div
         [:button
-         {:on-click #(swap! state add-person)}
-         "Create"]
-        [:button {:on-click #(swap! state (comp add-person remove-person))} "Update"]
+         {:on-click #(swap! state add-person)} "Create"]
+        [:button {:on-click #(swap! state replace-person)} "Update"]
         [:button {:on-click #(swap! state remove-person)} "Delete"]
-        [:button {:on-click #(swap! state assoc :trie (time (init-trie (repeatedly 100000 random-person))))} "Load Test"]]])))
+        [:button {:on-click #(swap! state (comp index (fn [state] (update state :people
+                                                                          (fn [people] (time (into people (repeatedly 10000 random-person))))))))} "Load Test"]]])))
